8d13678e5167f9963c1e90be0f420200c201cdec
srh-golubski <srh.sexton@gmail.com>
2022-03-21 14:57:04 -0500

    Finished ArrrayList Exercise numbers

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayListExerciseNumbers.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayListExerciseNumbers.java
@@ -1,2 +1,12 @@
-package org.launchcode.java.demos.lsn2controlflowandcollections;public class ArrayListExerciseNumbers {
+package org.launchcode.java.demos.lsn2controlflowandcollections;
+
+public class ArrayListExerciseNumbers {
+    public static void main(String[] args) {
+        int[] numbersArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int numbersSum = 0;
+        for (int i = 0; i < numbersArray.length; i++) {
+            numbersSum += numbersArray[i];
+        }
+        System.out.println(numbersSum);
+    }
 }
