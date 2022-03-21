f164195c8f059459d36bb7d1a2e6183878dc9654
srh-golubski <srh.sexton@gmail.com>
2022-03-21 14:56:22 -0500

    Finished Array Exercise Sentence

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayExerciseSentence.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayExerciseSentence.java
@@ -1,2 +1,11 @@
-package org.launchcode.java.demos.lsn2controlflowandcollections;public class ArrayExerciseSentence {
+package org.launchcode.java.demos.lsn2controlflowandcollections;
+
+public class ArrayExerciseSentence {
+    public static void main(String[] args) {
+        String sentence = "I would not, could not, in a box. I would not, could not, with a fox. I will not eat them in a house. I will not eat them with a mouse";
+        String[] sentenceArray = sentence.split("");
+        for (String a : sentenceArray) {
+            System.out.println(a);
+        }
+    }
 }
